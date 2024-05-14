1b54bf1298c5d89be606064d8cbf14f7197abbdd
PiersiM <piersilbio.marcolin@rse-web.it>
2024-05-14 15:19:26 +0200

    bugfix

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8001,
+        port=8000,
         log_level="debug",
         reload=False,
         lifespan="on",
